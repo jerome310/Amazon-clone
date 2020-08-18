6255a13ac5b9936ace0dff0683230ef3a3abe09d
Jerome <dreammedia365@yahoo.com>
2020-08-18 12:50:01 -0700

    Updates

---
+++ precache-manifest.49d1fab38f0864e66153cf9523d78ea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451d68f93e23c27cc3d91c6a4947a5e1",
+    "url": "/Amazon-clone/index.html"
+  },
+  {
+    "revision": "da175f74ad97c148be4e",
+    "url": "/Amazon-clone/static/css/main.8ff5085c.chunk.css"
+  },
+  {
+    "revision": "d1fefef9ca871d956331",
+    "url": "/Amazon-clone/static/js/2.3bf5b161.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Amazon-clone/static/js/2.3bf5b161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da175f74ad97c148be4e",
+    "url": "/Amazon-clone/static/js/main.034d368a.chunk.js"
+  },
+  {
+    "revision": "b4ca44839ab564cdfff3",
+    "url": "/Amazon-clone/static/js/runtime-main.a692d69b.js"
+  }
+]);
